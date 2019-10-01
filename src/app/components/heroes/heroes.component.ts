960f9af09d3e84620653c3eb123c8b682a11a801
JlFerrete <joseluisferrete@gmail.com>
2019-10-01 22:18:09 +0200

    Created service for Heroes

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from "@angular/core";
 
+import { HeroesService, Heroe } from "../../services/heroes.service";
+
 @Component({
   selector: "app-heroes",
   templateUrl: "./heroes.component.html"
 })
 export class HeroesComponent implements OnInit {
-  constructor() {}
+  heroes: Heroe[] = [];
 
-  ngOnInit() {}
+  constructor(private _heroesService: HeroesService) {
+    console.log("constructor");
+  }
+
+  ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+
+    console.log(this.heroes);
+  }
 }
