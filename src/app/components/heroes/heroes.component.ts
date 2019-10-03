e28ec39bc65677f475d5206a2913b66a8116822c
JlFerrete <joseluisferrete@gmail.com>
2019-10-03 19:39:48 +0200

    Router

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-
 import { HeroesService, Heroe } from "../../services/heroes.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-heroes",
@@ -9,13 +9,17 @@ import { HeroesService, Heroe } from "../../services/heroes.service";
 export class HeroesComponent implements OnInit {
   heroes: Heroe[] = [];
 
-  constructor(private _heroesService: HeroesService) {
-    console.log("constructor");
+  constructor(private _heroesService: HeroesService, private router: Router) {
+    // console.log("constructor");
   }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
 
-    console.log(this.heroes);
+    // console.log(this.heroes);
+  }
+
+  verHeroe(idx: number) {
+    this.router.navigate(["/heroe", idx]);
   }
 }
