92dbaf1d3a97a12d7ff6a84e95c0097c46e96b6a
JlFerrete <joseluisferrete@gmail.com>
2019-10-08 22:49:17 +0200

    Done Buscador

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html"
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
 
   buscarHeroe(termino: string) {
-    console.log(termino);
+    // console.log(termino);
+    this.router.navigate(["/buscar", termino]);
   }
 }
